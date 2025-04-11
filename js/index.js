28ba3320c7297b71a066dd0603b74f2979f2e3d6
Yulia Bondar <yuliabondar98@gmail.com>
2025-04-11 23:43:21 +0300

    feat: remove infinite mode from slider

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
   if ($(window).width() > 768) {
     $('.projects').slick({
-      infinite: true,
       slidesToShow: 3,
       slidesToScroll: 1,
       responsive: [
